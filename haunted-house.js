9b8f228044f5f42c1f4f86de8b2198c79179d608
Student <student@Admins-MacBook-Air.local>
2018-07-11 17:00:26 -0700

    MEME REVIEW

--- haunted-house.js
+++ haunted-house.js
@@ -1,18 +1,22 @@
-// Author: FirstName LastName
+// Author: david estrada
 var readline = require("readline-sync");
 
 console.log();
 console.log("*****************************************************************");
-console.log("                          HAUNTED HOUSE");
-console.log("                 A game by FirstName LastName");
+console.log("                          Meme review");
+console.log("                 A game by DAVID  ESTRADA");
 console.log("*****************************************************************");
-console.log("You are standing in front of a spooky haunted house.");
-console.log("You hear what sounds like screaming coming from inside the house.");
-var enterHouse = readline.question("Will you enter the house? (yes or no) ");
-if(enterHouse == "y" || enterHouse == "yes") {
+console.log("You are a person who enjoys memes and you want to review them.");
+console.log("You see your the Monkey Hair Cut Monkey meme and see that it is quite unique. You can review it and give it a positive or negative score. ");
+var reviewMeme = readline.question("Will you review it with a positive score? (yes or no) ");
+if(reviewMeme == "y" || reviewMeme == "yes") {
   // your code here
-
+console.log("The site you reviewed it on agrees with you.")
 } else {
   // your code here
-
+  if(reviewMeme == "n" || reviewMeme == "no") {
+    // your code here
+  console.log("The site you reviewed it on dis  agrees with you.")
+  }
 }
+console.log("");
